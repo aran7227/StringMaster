cd8e1d04d1c2a7458ddf915b35406608815ea6e5
aran7227 <161644211+aran7227@users.noreply.github.com>
2024-03-03 13:44:14 +0330

    wordFinder done

--- Base Structure/src/main/java/Warmup.java
+++ Base Structure/src/main/java/Warmup.java
@@ -3,14 +3,60 @@ public class Warmup {
     /**
      * Goal : Simple Introduction To Strings
      * In the first function, your inputs are a number and a sentence
+     *
      * @return is the number th word of the sentence
      */
     public String wordFinder(String sentence, int number) {
-        return null;
+        /*int nk=0;
+        for (int i=0;i<sentence.length();i++) {
+            if (sentence.charAt(i)==' ') {
+                nk++;
+            }
+        }
+        nk++;
+        String re="";
+        int j=0;
+        if (number>0 && number<=nk) {
+            int y = 1;
+            for (int i = 0; i < sentence.length(); i++) {
+                if (y == number && number == 1) {
+                    if (sentence.charAt(i) == ' ') {
+                        break;
+                    }
+                    re.charAt(j)=sentence.charAt(i);
+                    j++;
+                } else {
+                    if (y==number) {
+                        re.charAt(j)=sentence.charAt(i);
+                        j++;
+                        if (sentence.charAt(i+1)==' ') {
+                            break;
+                        }
+                    }
+                    if (sentence.charAt(i)==' ') {
+                        y++;
+                    }
+                }
+            }
+        } else {
+            System.out.print("number = "+number+" is out Of Bound");
+        }
+        return re;*/
+        String ans = "";
+        String[] a = sentence.split(" ");
+        if (number > a.length || number<=0) {
+            ans = " Number = " + number + " is out Of Bound";
+        }
+        else {
+            ans = a[number - 1];
+        }
+        return ans;
+
     }
 
     /**
      * Goal : Basic introduction to Strings & using foreach
+     *
      * @param number        is in String type
      * @param searchForEven is a boolean entry
      * @return if searchForEven is true ? return the number of even numbers : return the number of odd numbers
